29ece8611f5b6cfe6381a294ed6df9d4a140907a
mavirys <113904891+maxim007mv@users.noreply.github.com>
2024-10-14 23:55:01 +0300

    Add files via upload

---
+++ foto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foto',
+  standalone: true,
+  imports: [],
+  templateUrl: './foto.component.html',
+  styleUrl: './foto.component.css'
+})
+export class FotoComponent {
+
+}
